23eb426f8fe542707d9792e98d61f73ed5504fe9
IceSu .- <ice3215231@gmail.com>
2024-10-30 16:06:28 +0700

    update

--- web_stock/index.py
+++ web_stock/index.py
@@ -6,18 +6,20 @@ import module.page2 as page2
 import module.page3 as page3
 import module.page4 as page4
 
+st.set_page_config(page_title="Stock", 
+                   layout="wide")
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Sale_forecast','Report','Transform'], 
+    selected = option_menu("Main Menu", ["Home", 'Edit_stock','Shipping_stock','Transform'], 
         icons=['house', 'receipt','flag','file-earmark-arrow-up'], menu_icon="cast", default_index=0)
 
 if selected == 'Home':
     page1.menu_1()
 
-elif selected == 'Sale_forecast':
+elif selected == 'Edit_stock':
     page2.menu_2()
 
-elif selected == 'Report':
+elif selected == 'Shipping_stock':
     page3.menu_3()
 
 elif selected == 'Transform':
